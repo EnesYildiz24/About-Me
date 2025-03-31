761a3920be14b2069ee313c5e0f747ce9a9432af
EnesYildiz24 <deine.email@example.com>
2025-03-31 15:21:18 +0200

    published online

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,26 +1,24 @@
+// App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import WelcomePage from './pages/WelcomePage';
 import AboutMe from './pages/AboutMe';
 import ProjectsPage from './pages/ProjectsPage';
-import './AboutMe.css';
 import ContactPage from './pages/ContactPage';
 import QuizPage from './pages/QuizPage';
 import PasswordPage from './pages/PasswordPage';
-
+import './AboutMe.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/100600" element={<QuizPage />} />
-        <Route path="/password" element={<PasswordPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/about" element={<AboutMe />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/100600" element={<QuizPage />} />
+      <Route path="/password" element={<PasswordPage />} />
+    </Routes>
   );
 };
 
--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Wichtig für das Collapse (Hamburger-Menü)
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  console.error("Kein Root-Element mit ID 'root' gefunden!");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
+}
